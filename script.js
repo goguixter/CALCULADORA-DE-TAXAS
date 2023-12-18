1623d3b3addf99c64428800e49e2d7f6819811f7
goguixter <goguixter@gmail.com>
2023-12-18 11:03:55 -0300

    PRIMEIRO COMMIT - PROJETO CALCULADORA DE TAXAS

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcTaxes(){
+    const originalValue = document.querySelector('.value-input');
+    const installmenteQtd = document.querySelector('.qtd-input');
+    console.log(originalValue.value * installmenteQtd.value)
+}
+    
+
+
