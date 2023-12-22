98c651e66def86b728ce42137a9650e628a8b839
goguixter <goguixter@gmail.com>
2023-12-22 14:20:59 -0300

    ALGUNS AJUSTES NO INNER HTML

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function calcTaxes(){
-    const originalValue = document.querySelector('.value-input');
-    const installmenteQtd = document.querySelector('.qtd-input');
-    console.log(originalValue.value * installmenteQtd.value)
-}
+const calcButton = document.getElementById("calcBtn").addEventListener("click", totalValue)
+const inputValue = document.getElementById("value-input")
+const divResult = document.querySelector(".final-result")
+
+function totalValue(){
     
+    const result = inputValue.value * selectValue.value
+
+    divResult.innerHTML = `<p class="description"> Sua compra terá o valor total de<bR>${result+inputValue}</p>`
+    
+}
 
 
+const selectValue = document.querySelector(".select-installments")
+
+selectValue.addEventListener("change", totalValue)
